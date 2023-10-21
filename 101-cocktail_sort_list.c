51769572be5008ea36185753bf1ed04670336af6
Colin <clnearle@gmail.com>
2023-10-21 07:49:47 -0700

    added the cocktail sort

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,76 @@
+#include "sort.h"
+
+/**
+ * swap_nodes - Swaps two nodes in a linked list
+ * @list: Pointer to head
+ * @left: left swap
+ * @right: right swap
+ */
+void swap_nodes(listint_t **list, listint_t *left, listint_t *right)
+{
+	if (left->prev)
+	{
+		left->prev->next = right;
+	}
+	if (right->next)
+	{
+		right->next->prev = left;
+	}
+	left->next = right->next;
+	right->prev = left->prev;
+	left->prev = right;
+	right->next = left;
+	if (!right->prev)
+	{
+		*list = right;
+	}
+}
+
+/**
+ * cocktail_sort_list - sorts a doubly linked
+ * list of integers in ascending
+ * @list: head of list
+ */
+void cocktail_sort_list(listint_t **list)
+{
+	int swap = 1;
+	listint_t *st = *list;
+	listint_t *end = NULL;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	while (swap)
+	{
+		swap = 0;
+		while (st->next != end)
+		{
+			if (st->n > st->next->n)
+			{
+				swap_nodes(list, st, st->next);
+				print_list(*list);
+				swap = 1;
+			}
+			else
+			{
+				st = st->next;
+			}
+		}
+		if (!swap)
+			break;
+		swap = 0;
+		end = st;
+		while (st->prev != NULL)
+		{
+			if (st->n < st->prev->n)
+			{
+				swap_nodes(list, st->prev, st);
+				print_list(*list);
+				swap = 1;
+			}
+			else
+			{
+				st = st->prev;
+			}
+		}
+	}
+}
