5542d9e611a6ba2f466f389fa8dbf08c882b1be1
Colin <clnearle@gmail.com>
2023-10-23 10:08:03 -0700

    task 9

---
+++ 105-radix_sort.c
@@ -0,0 +1,76 @@
+#include "sort.h"
+
+/**
+ * find_max - Returns max amount of elems
+ * @array: array
+ * @size: size of array
+ * Return: max elem in array
+ */
+int find_max(int *array, size_t size)
+{
+	int maximum = array[0];
+	size_t i;
+
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > maximum)
+		{
+			maximum = array[i];
+		}
+	}
+	return (maximum);
+}
+/**
+ * count_sort - sorts array using counting
+ * algorithm
+ * @array: array
+ * @size: size of array
+ * @exp: current exponent
+ */
+void count_sort(int *array, size_t size, int exp)
+{
+	int *output = malloc(sizeof(int) * size);
+	int counter[10] = {0};
+	int i;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		counter[(array[i] / exp) % 10]++;
+	}
+	for (i = 1; i < 10; i++)
+	{
+		counter[i] += counter[i - 1];
+	}
+	for (i = (int)size - 1; i >= 0; i--)
+	{
+		output[counter[(array[i] / exp) % 10] - 1] = array[i];
+		counter[(array[i] / exp) % 10]--;
+	}
+	for (i = 0; i < (int)size; i++)
+	{
+		array[i] = output[i];
+	}
+	print_array(array, size);
+	free(output);
+}
+/**
+ * radix_sort - sorts an array of
+ * integers in ascending order
+ * @array: array
+ * @size: size of array
+ */
+void radix_sort(int *array, size_t size)
+{
+	int maximum;
+	int exp;
+
+	maximum = find_max(array, size);
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+	for (exp = 1; maximum / exp > 0; exp *= 10)
+	{
+		count_sort(array, size, exp);
+	}
+}
